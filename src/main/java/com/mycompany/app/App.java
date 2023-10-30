34b7cf6da7d4b78e295492ec8131194ef994bde7
Matías <43122214+matiasmscl@users.noreply.github.com>
2023-10-30 20:50:46 -0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,9 @@ public class App {
   
 
   public static String triangulo(int a, int b, int c) {
+    if (a==b && b==c && c!=0){
+      return "es un triangulo equilatero";
+    }
     return "error no es un triangulo";
   } 
   
